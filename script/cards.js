ab9c7e399a0e35a7c6fb7ce3c190759ba8d972fb
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-02-24 16:52:06 +0300

    Add files via upload

---
+++ script/cards.js
@@ -0,0 +1,27 @@
+// Коробка с картами сайта
+const initialCards = [
+  {
+    name: 'Франция',
+    link: 'images/france.jpg'
+  },
+  {
+    name: 'Германия',
+    link: 'images/germany.jpg'
+  },
+  {
+    name: 'Казахстан',
+    link: 'images/kazakhstan.jpg'
+  },
+  {
+    name: 'Португалия',
+    link: 'images/portugal.jpg'
+  },
+  {
+    name: 'Индонезия',
+    link: 'images/indonésie.jpg'
+  },
+  {
+    name: 'Швейцария',
+    link: 'images/switzerland.jpg'
+  }
+];
