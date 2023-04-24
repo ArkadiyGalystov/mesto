94248bacca6f63fab2b5c0cfcf395552e2a1487e
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-04-25 01:05:09 +0300

    Add files via upload

---
+++ src/components/UserInfo.js
@@ -0,0 +1,23 @@
+// Класс UserInfo отвечает за управление отображением информации о пользователе на странице
+class UserInfo {
+  constructor({ nameSelector, subtitleSelector }) {
+    this._profileTitle = document.querySelector(nameSelector);
+    this._profileSubtitle = document.querySelector(subtitleSelector);
+  }
+
+  // публичный метод getUserInfo, который возвращает объект с данными пользователя
+  getUserInfo() {
+    return {
+      name: this._profileTitle.textContent,
+      subtitle: this._profileSubtitle.textContent,
+    };
+  }
+
+  // публичный метод setUserInfo, который принимает новые данные пользователя и добавляет их на страницу
+  setUserInfo({ name, subtitle }) {
+    this._profileTitle.textContent = name;
+    this._profileSubtitle.textContent = subtitle;
+  }
+}
+
+export { UserInfo };
